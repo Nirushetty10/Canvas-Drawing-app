9bdd94fadfb3e0efcc611d25b74b5919b7a8cc86
Niranjan ks <109941827+Nirushetty10@users.noreply.github.com>
2023-05-25 12:28:20 +0530

    Delete Animations directory

--- Animations/index.js
+++
@@ -1,4 +0,0 @@
-function changeAnime() {
-    console.log(document.querySelector(".custom-animation").classList);
-    document.querySelector(".custom-animation").classList.toggle("custom-animation-visible")
-}
